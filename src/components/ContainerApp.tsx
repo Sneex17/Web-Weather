e0b396aecc3dd85695f6b53e9b20ef2c0bbdf6e5
Sneex17 <228738464+Sneex17@users.noreply.github.com>
2026-09-25 16:28:17 -0400

    Mejoras en el diseño de los inputs de busqueda

--- src/components/ContainerApp.tsx
+++ src/components/ContainerApp.tsx
@@ -1,36 +1,41 @@
 import React, { useState } from "react";
 import "../styles/ContainerApp.css";
-import "../styles/Forms.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import "../styles/Forms.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
 
 function ContainerApp() {
-const [ciudad, setCiudad] = useState('')
-
+  const [ciudad, setCiudad] = useState("");
 
   return (
     <div className="container">
       <div className="sub-container">
         <div className="container-buscar">
           <form action="">
-            <input 
-            type="text"
-            placeholder="Buscar Ciudad" 
-            value={ciudad}
-            onChange={(e)=> setCiudad(e.target.value)}
-            className="input-cities"/>
+            <input
+              type="text"
+              placeholder="Buscar Ciudad"
+              value={ciudad}
+              onChange={(e) => setCiudad(e.target.value)}
+              className="input-cities"
+            />
             <button className="btn-buscar">
-              <FontAwesomeIcon icon={faMagnifyingGlass}/>
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="icon-btn"/>
             </button>
           </form>
         </div>
         <div className="container-location">
+          <FontAwesomeIcon icon={faLocationDot} className="icon-location"/>
           <select name="paises" id="pais" className="select-pais">
+            <option value="" disabled selected>
+              Selecciones un pais
+            </option>
             <option>A</option>
             <option>A</option>
             <option>A</option>
-            <option>A</option>
-
           </select>
         </div>
         <div className="container-time">
@@ -43,9 +48,7 @@ const [ciudad, setCiudad] = useState('')
       </div>
       <div className="sub-container">
         <div className="temperatura card-radio"></div>
-        <div className="temperatura-detalles">
-
-        </div>
+        <div className="temperatura-detalles"></div>
       </div>
 
       <div className="sub-container">
